f5d1abe72c48c9ff8c417b844e7a832496475eac
lyubomyr83 <lyubomyr83@mail.ru>
2019-11-17 12:00:27 +0300

    rojer

--- rojer.py
+++ rojer.py
@@ -1,37 +1,41 @@
+from random import randint, choice
+
 print('Привет! Меня зовут Роджер. А как тебя?')
 name = input()
 name = name.title()
 print('Приятно познакомиться, '+name)
-print('''Давай проверим твои знания в математике
-Ты готов?(да или нет)''')
-ready = input()
-ready = ready.lower()
+print('Давай проверим твои знания в математике.')
 
+print('Хорошо')
+print(f'{name}, сколько примеров ты готов решить?')
 
-while ready not in {'да', 'нет'}:
-    print('''Должно быть да или нет.
-Введи заново''')
-    ready = input()
-    ready = ready.lower()
-
-
-if ready == 'да':
-    print('Хорошо')
-    print(f'{name}, сколько примеров ты готов решить?')
-
+questions_quantity = input()
+while not questions_quantity.isdigit():
+    print('Должна быть цифра!')
     questions_quantity = input()
-    while not questions_quantity.isdigit():
-        print('Должна быть цифра!')
-        questions_quantity = input()
 
-    print(f'До скольки будем считать?')
+print(f'До скольки будем считать?')
+max_answer = input()
+while not max_answer.isdigit():
+    print('Должна быть цифра!')
     max_answer = input()
-    while not max_answer.isdigit():
-        print('Должна быть цифра!')
-        max_answer = input()
 
+print('Хорошо, тогда начинаем...')
 
-else:
-    print('''Передумал? Хорошо, может как-нибудь в следующий раз...
-Пока!''')
+for i in range(int(questions_quantity)):
+    print(f'Пример {i+1}:')
 
+    number1 = randint(1, int(max_answer))
+    number2 = randint(1, int(max_answer))
+    sign = choice('+-')
+    print(f'Сколько будет {number1} {sign} {number2}?')
+
+    if sign == '-':
+        while number1 < number2:
+            number1 = randint(1, int(max_answer))
+            number2 = randint(1, int(max_answer))
+
+    if sign == '+':
+        while number1 + number2 > int(max_answer):
+            number1 = randint(1, int(max_answer))
+            number2 = randint(1, int(max_answer))
